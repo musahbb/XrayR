a85767cc6868797a1f98cf9f188e21aa008d8116
musahbb <80145128+musahbb@users.noreply.github.com>
2025-12-26 10:54:14 +0800

    Update import paths for XrayR common packages

--- app/mydispatcher/default.go
+++ app/mydispatcher/default.go
@@ -27,8 +27,8 @@ import (
 	"github.com/xtls/xray-core/transport"
 	"github.com/xtls/xray-core/transport/pipe"
 
-	"github.com/XrayR-project/XrayR/common/limiter"
-	"github.com/XrayR-project/XrayR/common/rule"
+	"github.com/musahbb/XrayR/common/limiter"
+	"github.com/musahbb/XrayR/common/rule"
 )
 
 var errSniffingTimeout = newError("timeout on sniffing")
